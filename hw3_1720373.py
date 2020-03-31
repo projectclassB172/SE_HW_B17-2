725e436a83219eec950c8b449cfccbf6d1cd042a
1720373盛昱淇 <61864296+dxmdppq@users.noreply.github.com>
2020-03-31 21:47:54 +0800

    Add files via upload

---
+++ hw3_1720373.py
@@ -0,0 +1,13 @@
+k = -9
+if k>= 0:
+    a = str(k) + "正数"
+    print('a')
+else:
+    print(str(k) + "负数")
+
+#2k = -9  2k命名不规范  只能以字母或下划线开头
+#  if 2k>= 0:   注意缩进
+#   with = 2k +"正数"  with命名不规范 with为特殊字符
+#        print(with)   注意缩进
+#   else:
+#    print(2K+ '负数") 标点使用错误应使用 ""
