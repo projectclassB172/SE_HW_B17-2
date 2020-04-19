82066db6b9e71c0b5d72bc4188445239ba7ac70d
1720369 <62129869+lfaimr@users.noreply.github.com>
2020-04-19 11:46:07 +0800

    Add files via upload

---
+++ homework4/Group2/hw4_1720369_2.py
@@ -0,0 +1,12 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:53:40) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> for a in range(30): 
+    for b in range(30):
+        if (a+b==30)and(2*a+4*b==90):
+                print("鸡有:%d;兔有:%d;"%(a,b))
+
+                
+鸡有:15;兔有:15;
+>>> 
+>>> 
+>>> 
